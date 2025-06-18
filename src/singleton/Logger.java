f7d793592430919b39add103d282395a2dbb620f
B-Surendar <surendarbalaraman2@gmail.com>
2025-06-18 22:11:26 +0530

    design pattern

---
+++ src/singleton/Logger.java
@@ -0,0 +1,16 @@
+package singleton;
+
+public class Logger {
+    private static Logger instance;
+
+    private Logger() {
+        System.out.println("Logger initialized.");
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+}
---
+++ src/singleton/Main.java
@@ -0,0 +1,17 @@
+package singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        System.out.println("Starting the application.");
+
+        Logger logger2 = Logger.getInstance();
+        System.out.println("Performing some operations.");
+
+        if (logger1 == logger2) {
+            System.out.println("Both logger instances are the same. Singleton verified.");
+        } else {
+            System.out.println("Different instances exist. Singleton failed.");
+        }
+    }
+}
