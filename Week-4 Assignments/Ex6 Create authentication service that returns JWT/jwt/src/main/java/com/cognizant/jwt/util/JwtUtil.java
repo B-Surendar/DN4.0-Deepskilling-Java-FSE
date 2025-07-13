331bccc10ea5d0d0a39f8464ab9863c3d3827e4a
B-Surendar <surendarbalaraman2@gmail.com>
2025-07-13 23:57:34 +0530

    Updated week 4

---
+++ Week-4 Assignments/Ex6 Create authentication service that returns JWT/jwt/src/main/java/com/cognizant/jwt/util/JwtUtil.java
@@ -0,0 +1,21 @@
+package com.cognizant.jwt.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "my-secret-key";
+    private static final long EXPIRATION = 1000 * 60 * 10; // 10 minutes
+
+    public static String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
